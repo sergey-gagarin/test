f2bf517a217c3afc8446d27d2580e09e5aabd3cf
serg <serg@crazyforms.com>
2011-09-04 16:00:32 +1000

    commit in b_01 ---- 01!

--- index.php
+++ index.php
@@ -3,3 +3,11 @@ print 'printing from test2 index file!';
 print "<a href='file02.php'>go file 2</a>";
 
 print "<a href='file03.php'>go file 3333</a>";
+
+echo "Some info added in branch b_01";
+echo "Some info added in branch b_01";
+echo "Some info added in branch b_01";
+echo "Some info added in branch b_01";
+echo "Some info added in branch b_01";
+echo "Some info added in branch b_01";
+echo "Some info added in branch b_01";
