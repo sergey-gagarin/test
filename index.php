b79c2fa7331bb068cbf79bfa85762a407a15794a
serg <serg@crazyforms.com>
2011-09-04 16:05:09 +1000

    commit to b_02 !!!!

--- index.php
+++ index.php
@@ -3,3 +3,13 @@ print 'printing from test2 index file!';
 print "<a href='file02.php'>go file 2</a>";
 
 print "<a href='file03.php'>go file 3333</a>";
+
+echo "b_02 b_02 more more more more ";
+echo "b_02 b_02 more more more more ";
+echo "b_02 b_02 more more more more ";
+echo "b_02 b_02 more more more more ";
+echo "b_02 b_02 more more more more ";
+echo "b_02 b_02 more more more more ";
+echo "b_02 b_02 more more more more ";
+echo "b_02 b_02 more more more more ";
+echo "b_02 b_02 more more more more ";
