f6882784d2f82d4fa8fbf5400fcaee032678693c
serg <serg@crazyforms.com>
2011-09-05 12:37:00 +1000

    the chnages

--- new01.php
+++ new01.php
@@ -1,3 +1,8 @@
 <?php
+echo'changes in set3333333333';
+echo'changes in set3333333333';
+echo'changes in set3333333333';
+echo'changes in set3333333333';
+echo'changes in set3333333333';
   // a new file !!!
 ?>
--- new02.php
+++ new02.php
@@ -1,3 +1,13 @@
 <?php
+echo'changes in set3333333333';
+echo'changes in set3333333333';
+echo'changes in set3333333333';
+echo'changes in set3333333333';
+echo'changes in set3333333333';
    // another new file!!
+   echo'changes in set3333333333';
+echo'changes in set3333333333';
+echo'changes in set3333333333';
+echo'changes in set3333333333';
+echo'changes in set3333333333';
 ?>
