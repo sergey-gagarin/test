f6afd88733fd5371d8361ebda364c1839cf50075
serg <serg@crazyforms.com>
2011-09-05 11:12:25 +1000

    05Sept

--- index.php
+++ index.php
@@ -5,23 +5,5 @@ print "<a href='file02.php'>go file 2</a>";
 print "<a href='file03.php'>go file 3333</a>";
 
 
-echo "Some info added in branch b_01";
-echo "Some info added in branch b_01";
-echo "Some info added in branch b_01";
-echo "Some info added in branch b_01";
-echo "Some info added in branch b_01";
-echo "Some info added in branch b_01";
-echo "Some info added in branch b_01";
-
-// merged with master !!!!!!!!!!!!!!
-
-echo "master master change!!!!";
-echo "master master change!!!!";
-echo "master master change!!!!";
-echo "master master change!!!!";
-echo "master master change!!!!";
-
-echo "master master change  22222 !!!!";
-
-echo "master master change  22222 !!!!";
+echo 'NEW test on 05-Mon_Sep 2001';
 
